cb6349326e2387a4c3fc5a5e9fcfc186e17c5fd2
Teun Leenders <t.j.tleenders@gmail.com>
2022-03-23 13:41:10 +0100

    drive.c hoppa

---
+++ MCEindopdracht/Eindopdracht/Eindopdracht/drive.c
@@ -0,0 +1,6 @@
+/*
+ * drive.c
+ *
+ * Created: 23/03/2022 13:40:29
+ *  Author: tjtle
+ */ 
--- MCEindopdracht/Eindopdracht/Eindopdracht/main.c
+++ MCEindopdracht/Eindopdracht/Eindopdracht/main.c
@@ -3,16 +3,26 @@
  *
  * Created: 23/03/2022 12:23:13
  * Author : tjtle
- */ 
+ */
 
+#define F_CPU 16e6
+
+#include <asf.h>
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 int main(void)
 {
     /* Replace with your application code */
+	
+	TCCR2 = 0b00000111; //initialize counter on portd.7
+
+	
     while (1) 
     {
+		OCR1A++;
+		if(OCR1A > 1550) OCR1A = 1350;
+		_delay_ms(100); 
     }
 }
 
