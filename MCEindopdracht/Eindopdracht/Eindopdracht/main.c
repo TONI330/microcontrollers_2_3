99bbd9ab64d79646c46f9f91dbff02e07505b77d
Teun Leenders <t.j.tleenders@gmail.com>
2022-03-23 12:24:21 +0100

    Created eindopdracht project

---
+++ MCEindopdracht/Eindopdracht/Eindopdracht/main.c
@@ -0,0 +1,18 @@
+/*
+ * Eindopdracht.c
+ *
+ * Created: 23/03/2022 12:23:13
+ * Author : tjtle
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
